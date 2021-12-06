7231c99775d0353c8d23875d0a52003a6cbe2fd7
SwallowMuysua <swallowmuysua@gmail.com>
2021-12-06 17:27:43 -0500

    con formulario de inicio

---
+++ Proyecto/Angular/FrontendPedidos/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { PaginaerrorComponent } from './plantilla/paginaerror/paginaerror.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent,
+    InicioComponent,
+    PaginaerrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Proyecto/Angular/FrontendPedidos/src/app/modulos/pedidos/asignar-pedidos/asignar-pedidos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AsignarPedidosComponent } from './asignar-pedidos.component';
+
+describe('AsignarPedidosComponent', () => {
+  let component: AsignarPedidosComponent;
+  let fixture: ComponentFixture<AsignarPedidosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AsignarPedidosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AsignarPedidosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
